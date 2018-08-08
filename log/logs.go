884386b3b28882054016e0191f00cae34d55ab2e
xieydd <xieydd@gmail.com>
2018-08-08 23:30:55 +0800

    log level format

---
+++ log/logs.go
@@ -0,0 +1,22 @@
+package log
+
+
+import (
+	"strings"
+	log "github.com/sirupsen/logrus"
+)
+
+func SetLogLevel(level string) {
+	switch strings.ToLower(level) {
+	case "debug":
+		log.SetLevel(log.DebugLevel)
+	case "info":
+		log.SetLevel(log.InfoLevel)
+	case "warn":
+		log.SetLevel(log.WarnLevel)
+	case "error":
+		log.SetLevel(log.ErrorLevel)
+	default:
+		log.Fatalf("Unknown level: %s",level)
+	}
+}
